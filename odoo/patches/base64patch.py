0e55cb5fa531fc4b8245dc09594d4cefd1f50bd4
Lorenzo <lorenzo.colombo@hyris.net>
2025-07-01 08:23:25 +0200

    missing alias

--- odoo/patches/base64patch.py
+++ odoo/patches/base64patch.py
@@ -1,3 +1,4 @@
 import base64
-#create alias to get python3.9 compatibility
+# create alias to get python3.9 compatibility
 base64.encodestring = base64.encodebytes
+base64.decodestring = base64.decodebytes
